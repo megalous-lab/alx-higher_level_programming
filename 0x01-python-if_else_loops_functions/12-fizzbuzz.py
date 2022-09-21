e65de0ccc75aef402c85a68165ffaee08f844deb
megalous-lab <yekeenabdmuizz@gmail.com>
2022-09-21 07:17:19 -0700

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print('Fizz', end=" ")
+            print('Fizz', end="")
         if i % 5 == 0:
-            print('Buzz', end=" ")
+            print('Buzz', end="")
         if i % 3 != 0 and i % 5 != 0:
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end="")
         print(end=" ")
