588305c6d9281517829fc7b96e455e7ea738e507
megalous-lab <yekeenabdmuizz@gmail.com>
2022-09-21 03:53:20 -0700

    Hot fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z") +1 ):
+for i in range(ord("a"), ord("z") + 1):
     if i == ord("e") or i == ord("q"):
         continue
     print("{:c}".format(i), end="")
