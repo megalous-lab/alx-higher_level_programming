b66a0314fb673f26142530689f2be50709264e0b
megalous-lab <yekeenabdmuizz@gmail.com>
2022-09-22 15:27:39 -0700

    Calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-	a = 10
-	b = 5
+    a = 10
+    b = 5
 
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
