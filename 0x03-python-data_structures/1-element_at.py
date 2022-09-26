798ad975f163ad4c8b7e8fd407f8b2443bc543ea
megalous-lab <yekeenabdmuizz@gmail.com>
2022-09-26 10:35:27 -0700

    Hot fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    if idx < 0:
-        return
-    elif idx > len(my_list) or idx == 5:
-        return
-    else:
-        return(my_list[idx])
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
