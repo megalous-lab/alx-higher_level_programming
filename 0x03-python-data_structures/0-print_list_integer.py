4c0e08cdecd23dfe46f1c3fd6ae8f2b92bb0c4e1
megalous-lab <yekeenabdmuizz@gmail.com>
2022-09-26 10:21:48 -0700

    Print integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    def print_list_integer(my_list=[]):
+        for print_list_integer in my_list:
+            print("{}".format(print_list_integer))
