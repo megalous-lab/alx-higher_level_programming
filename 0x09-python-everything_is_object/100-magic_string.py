5b3ec6e185ffe148c72658e428d4756c8da7d9de
megalous-lab <yekeenabdmuizz@gmail.com>
2022-10-26 05:11:55 -0700

    Python is everything

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(list_n=[]):
+    list_n += ["BestSchool"]
+    return ", ".join(list_n)
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def copy_list(li):
+    return li[:]
