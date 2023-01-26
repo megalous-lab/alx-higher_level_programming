02c13973fde3943f914b45186515547cb17b092c
megalous-lab <yekeenabdmuizz@gmail.com>
2023-01-26 08:23:04 -0800

    Tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,10 +3,8 @@ CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
-
-	INSERT INTO second_table (
-        (id, name, score) VALUES (1, "John", 10),
-        (id, name, score) VALUES (2, "Alex", 3),
-        (id, name, score) VALUES (3, "Bob", 14),
-        (id, name, score) Values (4, "George", 8));
 );
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) Values (4, "George", 8);
